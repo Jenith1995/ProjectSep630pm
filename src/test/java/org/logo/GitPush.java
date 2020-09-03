49981cb84bd02303917224c5a7b04b30bcfde1d8
Jenith1995 <jeni.28347@gmail.com>
2020-09-03 23:37:26 +0530

    Coding Push Done Successfully

---
+++ src/test/java/org/logo/GitPush.java
@@ -0,0 +1,17 @@
+package org.logo;
+
+public class GitPush {
+	
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Admin\\eclipse-workspace\\ProjectLogo\\Drivers\\chromedriver.exe");
+		
+		
+		
+	}
+		
+		
+
+	}
+
+
