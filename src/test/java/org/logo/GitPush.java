d9140a3c4ab8ab6bced3b01c63c7036c3cafaf1b
Jenith1995 <jeni.28347@gmail.com>
2020-09-07 05:29:49 -0700

    To Jenith

--- src/test/java/org/logo/GitPush.java
+++ src/test/java/org/logo/GitPush.java
@@ -2,11 +2,10 @@ package org.logo;
 
 public class GitPush {
 	
-	public static void main(String[] args) {
-		
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Admin\\eclipse-workspace\\ProjectLogo\\Drivers\\chromedriver.exe");
-		
-		
+	public static void main(String[] args) {		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Admin\\eclipse-workspace\\ProjectLogo\\Drivers\\chromedriver.exe");		
+		int a= 10;
+		int b = 45;
 		
 	}
 		
